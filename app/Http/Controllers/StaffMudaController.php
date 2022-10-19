437cdcee420f2b962efb55edd171cb8e2d5b7fab
triadi <triadilaksamana@gmail.com>
2022-10-19 23:02:06 +0700

    change annoucement method

--- app/Http/Controllers/StaffMudaController.php
+++ app/Http/Controllers/StaffMudaController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AnnouncementStaffMudaRequest;
 use App\Http\Requests\RegisterStaffMudaRequest;
 use App\Models\StaffMuda;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class StaffMudaController extends Controller
@@ -17,6 +17,7 @@ class StaffMudaController extends Controller
     public function store(RegisterStaffMudaRequest $request)
     {
         $validated = $request->validated();
+        $validated["unique_code"] = Str::random(8);
         $staff = StaffMuda::create($validated);
         if (!$staff) {
             return \redirect(route("staff-muda.index"))->with("error", "Error while registering");
@@ -31,12 +32,19 @@ class StaffMudaController extends Controller
         return Inertia::render("Sth/StaffMudaAnnouncement");
     }
 
-    public function show($nim)
+    public function show(AnnouncementStaffMudaRequest $request)
     {
+        $validated = $request->validated();
+        $nim = $validated["nim"];
+        $uniqueCode = $validated["unique_code"];
         $staffMuda = StaffMuda::where("nim", $nim)->first();
         if (!$staffMuda) {
             return back()
-                ->with("error", "Maaf, kamu tidak terdaftar pada peserta staff muda KBMTI 2022.");
+                ->with("error", "Maaf, NIM kamu tidak terdaftar pada sistem kami.");
+        }
+        if ($staffMuda->unique_code !== $uniqueCode) {
+            return back()
+                ->with("error", "Kode unik yang kamu masukan salah.");
         }
         return inertia("Sth/StaffMudaAnnouncement", ["staff_muda" => $staffMuda]);
     }
